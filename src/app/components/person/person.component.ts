dab3cc4d2285457c8b3a091163b4425d88c6539b
Matias Bernardi <matias.bernardi.1@gmail.com>
2023-10-10 16:35:41 -0300

    Correccion y validacion dni

--- src/app/components/person/person.component.ts
+++ src/app/components/person/person.component.ts
@@ -2,8 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, Input, Inject } from '@angular/core';
 import { Person } from './../../models/person.model';
 import {
-  MatDialog,
   MAT_DIALOG_DATA,
+  MatDialogClose,
   MatDialogRef,
 } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
@@ -16,6 +16,8 @@ import {
   ReactiveFormsModule,
 } from '@angular/forms';
 import { MaterialModule } from 'src/app/material.module';
+import { Response } from 'src/app/models/response.model';
+import { PersonsService } from 'src/app/services/persons/persons.service';
 
 @Component({
   standalone: true,
@@ -33,23 +35,82 @@ import { MaterialModule } from 'src/app/material.module';
   styleUrls: ['./person.component.css'],
 })
 export class PersonComponent {
-  // @Input() person!: Person;
   constructor(
     public dialogRef: MatDialogRef<PersonComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Person
+    @Inject(MAT_DIALOG_DATA) public data: Person,
+    public personService: PersonsService
   ) {}
 
-  selectedPerson = JSON.parse(JSON.stringify(this.data));
+  selectedPerson = Object.assign({}, this.data);
+  message: string = '';
 
-  emailFormControl = new FormControl('', [
+  dni = new FormControl(this.data.dni, [
+    Validators.required,
+    Validators.pattern('^[0-9]+$'),
+    Validators.minLength(7),
+  ]);
+
+  firstName = new FormControl(this.data.firstName, [
+    Validators.required,
+    Validators.pattern('^[a-zA-ZñÑá-úÁ-Ú]*$'),
+  ]);
+
+  lastName = new FormControl(this.data.lastName, [
+    Validators.required,
+    Validators.pattern('^[a-zA-ZñÑá-úÁ-Ú]*$'),
+  ]);
+
+  email = new FormControl(this.data.email, [
     Validators.required,
     Validators.email,
   ]);
 
-  nameFormControl = new FormControl('', [
-    Validators.required,
-    Validators.pattern('^[a-zA-ZñÑá-úÁ-Ú]*$'),
-  ]);
+  phone = new FormControl(this.data.phone, [Validators.pattern('^[0-9]+$')]);
+
+  birthdate = new FormControl(this.data.birthdate, [Validators.required]);
+
+  dataValidation() {
+    if (
+      this.dni.status === 'VALID' &&
+      this.firstName.status === 'VALID' &&
+      this.lastName.status === 'VALID' &&
+      this.email.status === 'VALID' &&
+      this.phone.status === 'VALID' &&
+      this.birthdate.status === 'VALID'
+    ) {
+      return false;
+    }
+    return true;
+  }
+
+  sendNewPerson() {
+    const newPerson = new Person(
+      this.data._id,
+      this.dni.value,
+      this.firstName.value,
+      this.lastName.value,
+      this.phone.value,
+      this.email.value,
+      this.birthdate.value
+    );
+
+    try {
+      this.personService.addOrEdit(newPerson).subscribe({
+        next: (res: Response) => {
+          this.message = res.message;
+          console.log(this.message);
+          console.log(JSON.stringify(this.data));
+          this.dialogRef.close();
+        },
+        error: (message) => {
+          this.message = message.message;
+          console.log(`Error: ${message.message}`);
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
--- src/app/services/persons/persons.service.ts
+++ src/app/services/persons/persons.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Person } from 'src/app/models/person.model';
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Response } from './../../models/response.model';
+
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -11,27 +14,32 @@ export class PersonsService {
   constructor(private http: HttpClient) {}
 
   //buscar paquete observable from rxjs
-  getAll() {
+  getAll(): Observable<Response> {
     return this.http.get<Response>('http://localhost:3000/api/person');
   }
 
-  addOrEdit(person: Person) {
-    if (person.id) {
-      return this.http.patch<Response>(
-        `http://localhost:3000/api/person/${person.id}`,
-        person
-      );
+  addOrEdit(person: Person): Observable<Response> {
+    if (person._id) {
+      return this.http
+        .patch<Response>(
+          `http://localhost:3000/api/person/${person._id}`,
+          person
+        )
+        .pipe(catchError(this.handleError));
     } else {
-      return this.http.post<Response>(
-        'http://localhost:3000/api/person',
-        person
-      );
+      return this.http
+        .post<Response>('http://localhost:3000/api/person', person)
+        .pipe(catchError(this.handleError));
     }
   }
 
   delete(person: Person) {
     return this.http.delete<Response>(
-      `http://localhost:3000/api/person/${person.id}`
+      `http://localhost:3000/api/person/${person._id}`
     );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    return throwError(() => new Error(error.error.message));
   }
 }
